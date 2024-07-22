d125e8d50d168546fdce25dd4b5dda58c4f72a02
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-07-22 21:59:01 +0530

    sent a session msg with return in controller,

--- app/Http/Controllers/empController.php
+++ app/Http/Controllers/empController.php
@@ -21,8 +21,8 @@ class empController extends Controller
             'mobile' => $mobile,
         ]);
 
-        $records=DB::table('employee')->get();
-        return view('empForm',['data'=>$records]);
+        $records = DB::table('employee')->get();
+        return view('empForm', ['data' => $records])->with('msg', 'Inserted Successfully');
         //return redirect()->back()->with('msg', 'Inserted Successfully');
     }
 }
