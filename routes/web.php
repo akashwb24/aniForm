5928d96cd1f83c18c0cf346b6d34cc8b1a4a72d5
Akash-Kumar-Prasad <iakashprasad@gmail.com>
2024-07-27 19:18:55 +0530

    CONTINUED>=37

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('dform', 'empForm');
 Route::post('pform', [empController::class, 'setdata']);
+
+//CONTINUE FROM 37 NUMBER VIDEO, PAGINATION START, ANILAB LAST ENCOUNTERED PROJECT CREATED
